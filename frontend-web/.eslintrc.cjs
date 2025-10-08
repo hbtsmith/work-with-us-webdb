6054e6e653ec23326627c6e36f8e95a200115c46
Herbert Smith <smith.hbt@gmail.com>
2025-10-08 12:19:34 -0300

    fix: simplify ESLint config to avoid missing @typescript-eslint/recommended
    - Remove @typescript-eslint/recommended from extends
    - Add @typescript-eslint to plugins array
    - Keep TypeScript parser and rules
    - Add node_modules to ignorePatterns
    - Disable base no-unused-vars in favor of TypeScript version
    
    Fixes: ESLint couldn't find the config '@typescript-eslint/recommended'

--- frontend-web/.eslintrc.cjs
+++ frontend-web/.eslintrc.cjs
@@ -3,18 +3,18 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off', // Desabilitar regra base em favor da do TypeScript
   },
 }
